c210e6d13057690a1cc1907e90c65e34999a189a
MU <m.iouriev@gmail.com>
2024-01-17 18:55:02 +0700

    2я лекция без 2го семинара

---
+++ lesson/Lesson_2.py
@@ -0,0 +1,61 @@
+# # # # # list_1 = []
+# # # # # list_1 = list()
+# # # # # list_1 = [1, 2, 3]
+# # # # # print(*list_1)
+
+# # # # # # for i in list_1:
+# # # # # #     print(i)
+    
+# # # # # #print(len(list_1))
+
+# # # # # print(list_1[0])
+
+# # # # ##
+
+# # # # list = [1, 5]
+# # # # print(list)
+# # # # list.append(8)
+# # # # print(list)
+# # # # list.append(8435)
+# # # # print(list)
+
+# # # list = []
+# # # print(list)
+# # # for i in range(5):
+# # #     list.append(i)
+# # #     print(list) 
+
+# # #list.pop() - удаляет последний элемент, но если в скобках указать индекс элемент в массиве - удалится элемент с этим индексом
+
+# # #list.insert() - Добовляет элемент в список. принимает 2 аргумента: позицию на которую надо добавить элемент, значение которое нужно добавить
+
+# # ## Кортеж - tuple
+
+# # # t = ()
+
+# # # print(type(t))
+
+# # # t = (1,)
+# # # print(type(t)) # type - показывает какого типа выводимая в консоль переменная
+
+# # v = [1, 2, 3]
+# # print(type(v))
+
+# # v = tuple(v)
+# # print(type(v))
+# # print(v)
+     
+# # a,b,c = v
+
+# # print(a, b, c)
+
+# ## Словарь - dictionary
+
+# d = {}
+# d = dict()
+
+# d['q'] = 'qwerty'
+# print(d)
+
+# d['w'] = 'werty'
+# print(d['q'])

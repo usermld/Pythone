088412127b69a527f7c08d1e473403c140f0881e
MU <m.iouriev@gmail.com>
2024-01-24 22:28:33 +0700

    это дз заняло несколько дней *****

--- 18.py
+++ 18.py
@@ -1,14 +1,23 @@
-list_1 = [1, 2, 3, 4, 5]
-list_2 = []
-list_3 = []
+list_1 = [1, 2, 3, 4, 6]
 k = 6
-i = 0
+# i = 0
+# j = 1
+# count = 0
+# count_2 = 0
 
-while i < len(list_1):
-    if k <= list_1[i]:
-        list_1[i].add(list_2)
-        i += 1
-    else:
-        list_1[i].add(list_3)
-        i += 1
-        
+
+# while i < len(list_1):
+#     count = k - list_1[i]
+#     count_2 = k - list_1[j]
+    
+#     if count < 0:
+#         count *= -1
+#     if count_2 < 0:
+#         count_2 *= -1
+    
+#     i += 1
+
+def closest(list_1, k):
+     
+    return list_1[min(range(len(list_1)), key = lambda i: abs(list_1[i]-k))]
+print(closest(list_1, k))
